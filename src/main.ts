66797f117627606d9e48d12b346498f46591e46a
Igor Bessa <ibessa@ucsc.edu>
2024-11-04 18:06:15 -0800

    Pushing first changes to main.ts for L16

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,18 @@
 // todo
+import "./style.css";
+
+const APP_NAME = "GeoCoin";
+const app = document.querySelector<HTMLDivElement>("#app")!;
+document.title = APP_NAME;
+app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+app.append(header);
+
+const tempButton = document.createElement("button");
+tempButton.innerHTML = "Click me";
+app.append(tempButton);
+
+tempButton.addEventListener("click", () => {
+  alert("you clicked the button");
+});
