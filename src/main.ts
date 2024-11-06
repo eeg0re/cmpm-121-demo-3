ebe0930288b6f9fffd1df9b6eca991fd0820adb5
Igor Bessa <ibessa@ucsc.edu>
2024-11-06 14:31:11 -0800

    pushing last changes before class

--- src/main.ts
+++ src/main.ts
@@ -13,10 +13,9 @@ app.innerHTML = APP_NAME;
 const header = document.createElement("h1");
 app.append(header);
 
-//const STARTING_POS = leaflet.latLng(36.59222, -122.03460);
 const STARTING_POS = leaflet.latLng(36.98949379578401, -122.06277128548504);
 
-const ZOOM_LVL: number = 18;
+const ZOOM_LVL: number = 19;
 //const CELL_SIZE: number = 0.0001;          // number of degrees in a cell
 
 const map: leaflet.Map = leaflet.map("map", { // create our map starting at Oakes Classroom
@@ -28,8 +27,13 @@ const map: leaflet.Map = leaflet.map("map", { // create our map starting at Oake
   scrollWheelZoom: false,
 });
 
+// add the world background to the leaflet map
 leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   maxZoom: 19,
   attribution:
     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
+
+const player = leaflet.marker(STARTING_POS);
+player.bindTooltip("You are here");
+player.addTo(map);
